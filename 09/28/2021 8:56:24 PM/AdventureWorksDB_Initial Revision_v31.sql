193a1c5dc1786349b72595f727b653d03d394713
sqldbmgitrepo <88746445+sqldbmgitrepo@users.noreply.github.com>
2021-09-28 23:56:25 -0400

    Generated by SqlDBM: AdventureWorksDB - v31 by "priyank.singh@sqldbm.com"

---
+++ 09/28/2021 8:56:24 PM/AdventureWorksDB_Initial Revision_v31.sql
@@ -0,0 +1,15 @@
+-- *********************** SqlDBM: Snowflake ************************
+-- ******************************************************************
+
+CREATE FILE FORMAT fileFormat_431
+ TYPE = JSON;
+
+
+-- ************************************** EDW."Test Table"
+CREATE TABLE EDW."Test Table"
+(
+ name number(38,0) NOT NULL,
+ CONSTRAINT PK_496 PRIMARY KEY ( name )
+);
+
+
