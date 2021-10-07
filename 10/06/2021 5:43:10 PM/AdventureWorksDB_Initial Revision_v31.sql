75b895edc423e37ebcdbfafacfa421402857a5f7
sqldbmgitrepo <88746445+sqldbmgitrepo@users.noreply.github.com>
2021-10-06 20:43:11 -0400

    Generated by SqlDBM: AdventureWorksDB - v31 by "priyank.singh@sqldbm.com"

---
+++ 10/06/2021 5:43:10 PM/AdventureWorksDB_Initial Revision_v31.sql
@@ -0,0 +1,47 @@
+-- *********************** SqlDBM: Snowflake ************************
+-- ******************************************************************
+
+CREATE FILE FORMAT fileFormat_431
+ TYPE = JSON;
+
+
+-- ************************************** EDW.Country
+CREATE TABLE EDW.Country
+(
+ countryId numeric NOT NULL,
+ cityId    numeric NOT NULL,
+ city      varchar(50) NOT NULL,
+ CONSTRAINT PK_country PRIMARY KEY ( countryId )
+);
+
+
+-- ************************************** EDW.City
+CREATE TABLE EDW.City
+(
+ cityId    numeric NOT NULL COMMENT 'Unique identification',
+ city      varchar NOT NULL COMMENT 'Name of the city',
+ countryId numeric NOT NULL COMMENT 'id of the country where city belongs to',
+ CONSTRAINT PK_city PRIMARY KEY ( cityId ),
+ CONSTRAINT FK_452 FOREIGN KEY ( countryId ) REFERENCES EDW.Country ( countryId )
+)
+COMMENT = 'Capturing city information all over world';
+
+
+-- ************************************** EDW.SampleView
+Create View EDW.SampleView
+
+  Comment = 'This is SampleView Comment'
+
+AS
+(
+    -- Please type View Definition here
+    SELECT 
+           FinanceKey, 
+           OrganizationKey,  
+           AccountKey, 
+           ScenarioKey
+    FROM 
+        EDW.FactFinance ff
+         
+);
+
